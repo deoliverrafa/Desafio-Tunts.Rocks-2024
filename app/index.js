f5109f589051cb721ea82cfe168a913998ca8217
deoliverrafa <ytsecretgames38@gmail.com>
2024-02-02 16:35:53 -0300

    getAuthSheets function

--- app/index.js
+++ app/index.js
@@ -4,5 +4,28 @@ const app = express()
 let port;
 
 
+async function getAuthSheets() {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    })
+
+    const client = await auth.getClient();
+
+    const googlesheets = google.sheets({
+        version: 'v4',
+        auth: client
+    })
+
+    const spreadSheetId = '1kY_edYv_PehDUHOkWUfXeDYYgv3KBC5nJj7Af5iMyJM'
+
+    return {
+        auth,
+        googlesheets,
+        client,
+        spreadSheetId
+    }
+}
 
 app.listen(port = 3001, () => console.log(`Rodando na porta ${port}`))
